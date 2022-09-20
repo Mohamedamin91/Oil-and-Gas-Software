e68e7e886d97e828cec2c0490b6d2d533e9174fe
Mohammed Amin <m.amin@delmon.com.sa>
2022-09-20 23:11:24 +0300

    V3.0

--- Oil and Gas Software/Form1.Designer.cs
+++ Oil and Gas Software/Form1.Designer.cs
@@ -75,7 +75,7 @@ namespace Oil_and_Gas_Software
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
+            this.button2.Text = "Show";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
